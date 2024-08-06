f67b04c9a4bb7cd766e9129ed5209e357c666cbc
Sergey Gornih <seregaland@yandex.ru>
2024-08-06 13:54:08 +0300

    fix group

--- resenderbot.js
+++ resenderbot.js
@@ -8,22 +8,20 @@ const bot = new TelegramBot(token, {polling: true});
 const user1 = process.env.USER1
 const user2 = process.env.USER2
 
-//const group1 = process.env.GROUP1
-//const group2 = process.env.GROUP2
+const group1 = process.env.GROUP1
+const group2 = process.env.GROUP2
 
-let group1 = '-4249083059' 
-let group2 = '-4271241131'
+const group3 = process.env.GROUP3
+const group4 = process.env.GROUP4
+
+const group5 = process.env.GROUP5
+const group6 = process.env.GROUP6
+
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
-    const firstname = msg.from.first_name
-    const lastname = msg.from.last_name
     const text = msg.text ? msg.text : '';
-    const messageId = msg.message_id;
     const fromId = msg.from.id;
-    const isBot = msg.from.is_bot;
-
-    //if (text.includes('http://'))sfsdfsdzczxczx
 
     console.log(msg)
 
@@ -31,25 +29,30 @@ bot.on('message', async (msg) => {
         console.log('/start')
     }
 
-    if (text === 'тест2') {
-        console.log('тест2')
-        if (group1.length === 0) {
-            group1 = chatId
-        } else {
-            group2 = chatId
-        }
-    }
-
     //обработка документов
     if (msg.document) {
-        const image = msg.document.file_id
-        console.log(image)
+        const doc = msg.document.file_id
+        //console.log(image)
 
         if (chatId.toString() === group1) {
-            await bot.sendDocument(group2, image)
+            await bot.sendDocument(group2, doc)
         } 
         else if (chatId.toString() === group2) {
-            await bot.sendDocument(group1, image)
+            await bot.sendDocument(group1, doc)
+        }
+
+        if (chatId.toString() === group3) {
+            await bot.sendDocument(group4, doc)
+        } 
+        else if (chatId.toString() === group4) {
+            await bot.sendDocument(group3, doc)
+        }
+
+        if (chatId.toString() === group5) {
+            await bot.sendDocument(group6, doc)
+        } 
+        else if (chatId.toString() === group6) {
+            await bot.sendDocument(group5, doc)
         }
     }
 
@@ -67,6 +70,20 @@ bot.on('message', async (msg) => {
             await bot.sendPhoto(group1, image)
         }
 
+        if (chatId.toString() === group3) {
+            await bot.sendPhoto(group4, image)
+        } 
+        else if (chatId.toString() === group4) {
+            await bot.sendPhoto(group3, image)
+        }
+
+        if (chatId.toString() === group5) {
+            await bot.sendPhoto(group6, image)
+        } 
+        else if (chatId.toString() === group6) {
+            await bot.sendPhoto(group5, image)
+        }
+
     }
 
     //обработка аудио сообщений
@@ -79,6 +96,20 @@ bot.on('message', async (msg) => {
         else if (chatId.toString() === group2) {
             await bot.sendVoice(group1, voice)
         }
+
+        if (chatId.toString() === group3) {
+            await bot.sendVoice(group4, voice)
+        } 
+        else if (chatId.toString() === group4) {
+            await bot.sendVoice(group3, voice)
+        }
+
+        if (chatId.toString() === group5) {
+            await bot.sendVoice(group6, voice)
+        } 
+        else if (chatId.toString() === group6) {
+            await bot.sendVoice(group5, voice)
+        }
     }
 
     //обработка сообщений    
@@ -87,15 +118,36 @@ bot.on('message', async (msg) => {
         //let text2 = text.replace(/(?:https?):\/\/t.me[\n\S]+/g, '');
         let retext = text.replace(/(?:@)[\n\S]+/g, 'BitWire Support');
 
-
-        let conversation = []
-
-        
+        //1,2
         if (chatId.toString() === group1) {
             await bot.sendMessage(group2, retext)
         } 
         else if (chatId.toString() === group2) {
             await bot.sendMessage(group1, retext)
+        }
+
+        //3,4
+        if (chatId.toString() === group3) {
+            await bot.sendMessage(group4, retext)
+        } 
+        else if (chatId.toString() === group4) {
+            await bot.sendMessage(group3, retext)
+        }
+
+        //5,6
+        if (chatId.toString() === group5) {
+            await bot.sendMessage(group6, retext)
+        } 
+        else if (chatId.toString() === group6) {
+            await bot.sendMessage(group5, retext)
+        }
+
+        //test
+        if (chatId.toString() === user1) {
+            await bot.sendMessage(user2, retext)
+        } 
+        else if (chatId.toString() === user2) {
+            await bot.sendMessage(user1, retext)
         }
     }
 
