d82957c3bf421ccfc35b86b389874b260339fa74
Sergey Gornih <seregaland@yandex.ru>
2024-08-07 16:59:22 +0300

    fix add converation

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ const conversationController = require('../controllers/conversationController')
 // get USERS  
 router.get("/users", userController.getUsers);
 
+
+// CONVERSATION
+router.post('/conversation/add', conversationController.newConversation)
+router.get('/conversation/get/:id', conversationController.getConversation)
+router.get('/conversations/get', conversationController.getConversations)
+
 module.exports = router
