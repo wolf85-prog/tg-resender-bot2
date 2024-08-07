fef5faf3b68a30f5c9323c53356599a075b2c3ce
Sergey Gornih <seregaland@yandex.ru>
2024-08-07 14:40:51 +0300

    fix bd

---
+++ models/models.js
@@ -0,0 +1,42 @@
+const sequelize = require('../connections/db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    username: {type: DataTypes.STRING, allowNull: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
+
+const UserBot = sequelize.define('userbot', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    firstname: {type: DataTypes.STRING},
+    lastname: {type: DataTypes.STRING},
+    chatId: {type: DataTypes.STRING, unique: true},
+})
+
+const Message = sequelize.define('message', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    conversationId: {type: DataTypes.STRING},
+    senderId: {type: DataTypes.STRING},
+    receiverId: {type: DataTypes.STRING},    
+    text: {type: DataTypes.STRING}, //текст сообщения;
+    type: {type: DataTypes.STRING},      //тип сообщения;
+    is_bot: {type: DataTypes.BOOLEAN},
+    messageId: {type: DataTypes.STRING},
+})
+
+const Conversation = sequelize.define('r_conversation', {
+    members: {type: DataTypes.ARRAY(DataTypes.STRING)},
+})
+
+
+
+module.exports = {
+    User, 
+    UserBot, 
+    Message, 
+    Conversation, 
+
+}
--- resenderbot.js
+++ resenderbot.js
@@ -5,6 +5,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_API_TOKEN
 const bot = new TelegramBot(token, {polling: true});
 
+//подключение к БД PostreSQL
+const sequelize = require('./connections/db')
+const {UserBot, Message, Conversation} = require('./models/models');
+
 const user1 = process.env.GROUP12
 const user2 = process.env.GROUP22
 
@@ -25,131 +29,153 @@ bot.on('message', async (msg) => {
 
     console.log(msg)
 
-    if (text === '/start') {
-        console.log('/start')
+    try {
+        if (text === '/start') {
+            //добавить пользователя в бд
+            const user = await UserBot.findOne({where:{chatId: chatId.toString()}})
+            if (!user) {
+                await UserBot.create({ firstname: firstname, lastname: lastname, chatId: chatId })
+                console.log('Пользователь добавлен в БД')
+            } else {
+                console.log('Отмена добавления в БД. Пользователь уже существует')
+            }
+        }
+
+        //обработка документов
+        if (msg.document) {
+            const doc = msg.document.file_id
+            //console.log(image)
+
+            if (chatId.toString() === group1) {
+                await bot.sendDocument(group2, doc)
+            } 
+            else if (chatId.toString() === group2) {
+                await bot.sendDocument(group1, doc)
+            }
+
+            if (chatId.toString() === group3) {
+                await bot.sendDocument(group4, doc)
+            } 
+            else if (chatId.toString() === group4) {
+                await bot.sendDocument(group3, doc)
+            }
+
+            if (chatId.toString() === group5) {
+                await bot.sendDocument(group6, doc)
+            } 
+            else if (chatId.toString() === group6) {
+                await bot.sendDocument(group5, doc)
+            }
+        }
+
+        //обработка изображений
+        if (msg.photo) {
+            console.log(msg.photo)
+            //const image = await bot.getFile(msg.photo[msg.photo.length-1].file_id);
+            const image = msg.photo[0].file_id
+            console.log(image)
+
+            if (chatId.toString() === group1) {
+                await bot.sendPhoto(group2, image)
+            } 
+            else if (chatId.toString() === group2) {
+                await bot.sendPhoto(group1, image)
+            }
+
+            if (chatId.toString() === group3) {
+                await bot.sendPhoto(group4, image)
+            } 
+            else if (chatId.toString() === group4) {
+                await bot.sendPhoto(group3, image)
+            }
+
+            if (chatId.toString() === group5) {
+                await bot.sendPhoto(group6, image)
+            } 
+            else if (chatId.toString() === group6) {
+                await bot.sendPhoto(group5, image)
+            }
+
+        }
+
+        //обработка аудио сообщений
+        if (msg.voice) {
+            const voice = msg.voice.file_id
+
+            if (chatId.toString() === group1) {
+                await bot.sendVoice(group2, voice)
+            } 
+            else if (chatId.toString() === group2) {
+                await bot.sendVoice(group1, voice)
+            }
+
+            if (chatId.toString() === group3) {
+                await bot.sendVoice(group4, voice)
+            } 
+            else if (chatId.toString() === group4) {
+                await bot.sendVoice(group3, voice)
+            }
+
+            if (chatId.toString() === group5) {
+                await bot.sendVoice(group6, voice)
+            } 
+            else if (chatId.toString() === group6) {
+                await bot.sendVoice(group5, voice)
+            }
+        }
+
+        //обработка сообщений    
+        if ((text || '')[0] !== '/' && text) {
+
+            //добавить пользователя в бд
+            const user = await UserBot.findOne({where:{chatId: chatId.toString()}})
+            if (!user) {
+                await UserBot.create({ firstname: firstname, lastname: lastname, chatId: chatId })
+                console.log('Пользователь добавлен в БД')
+            } else {
+                console.log('Отмена добавления в БД. Пользователь уже существует')
+            }
+
+            let text2 = text.replace(/(?:https?):\/\/t.me[\n\S]+/g, '');
+            let retext = text2.replace(/(?:@)[\n\S]+/g, 'BitWire Support');
+
+            //1,2
+            if (chatId.toString() === group1) {
+                await bot.sendMessage(group2, retext)
+            } 
+            else if (chatId.toString() === group2) {
+                await bot.sendMessage(group1, retext)
+            }
+
+            //3,4
+            if (chatId.toString() === group3) {
+                await bot.sendMessage(group4, retext)
+            } 
+            else if (chatId.toString() === group4) {
+                await bot.sendMessage(group3, retext)
+            }
+
+            //5,6
+            if (chatId.toString() === group5) {
+                await bot.sendMessage(group6, retext)
+            } 
+            else if (chatId.toString() === group6) {
+                await bot.sendMessage(group5, retext)
+            }
+
+            //test
+            if (chatId.toString() === user1) {
+                await bot.sendMessage(user2, retext)
+            } 
+            else if (chatId.toString() === user2) {
+                await bot.sendMessage(user1, retext)
+            }
+        }
+    } catch (error) {
+        console.log('Произошла непредвиденная ошибка в боте Resender! ', error.message)
     }
 
-    //обработка документов
-    if (msg.document) {
-        const doc = msg.document.file_id
-        //console.log(image)
-
-        if (chatId.toString() === group1) {
-            await bot.sendDocument(group2, doc)
-        } 
-        else if (chatId.toString() === group2) {
-            await bot.sendDocument(group1, doc)
-        }
-
-        if (chatId.toString() === group3) {
-            await bot.sendDocument(group4, doc)
-        } 
-        else if (chatId.toString() === group4) {
-            await bot.sendDocument(group3, doc)
-        }
-
-        if (chatId.toString() === group5) {
-            await bot.sendDocument(group6, doc)
-        } 
-        else if (chatId.toString() === group6) {
-            await bot.sendDocument(group5, doc)
-        }
-    }
-
-    //обработка изображений
-    if (msg.photo) {
-        console.log(msg.photo)
-        //const image = await bot.getFile(msg.photo[msg.photo.length-1].file_id);
-        const image = msg.photo[0].file_id
-        console.log(image)
-
-        if (chatId.toString() === group1) {
-            await bot.sendPhoto(group2, image)
-        } 
-        else if (chatId.toString() === group2) {
-            await bot.sendPhoto(group1, image)
-        }
-
-        if (chatId.toString() === group3) {
-            await bot.sendPhoto(group4, image)
-        } 
-        else if (chatId.toString() === group4) {
-            await bot.sendPhoto(group3, image)
-        }
-
-        if (chatId.toString() === group5) {
-            await bot.sendPhoto(group6, image)
-        } 
-        else if (chatId.toString() === group6) {
-            await bot.sendPhoto(group5, image)
-        }
-
-    }
-
-    //обработка аудио сообщений
-    if (msg.voice) {
-        const voice = msg.voice.file_id
-
-        if (chatId.toString() === group1) {
-            await bot.sendVoice(group2, voice)
-        } 
-        else if (chatId.toString() === group2) {
-            await bot.sendVoice(group1, voice)
-        }
-
-        if (chatId.toString() === group3) {
-            await bot.sendVoice(group4, voice)
-        } 
-        else if (chatId.toString() === group4) {
-            await bot.sendVoice(group3, voice)
-        }
-
-        if (chatId.toString() === group5) {
-            await bot.sendVoice(group6, voice)
-        } 
-        else if (chatId.toString() === group6) {
-            await bot.sendVoice(group5, voice)
-        }
-    }
-
-    //обработка сообщений    
-    if ((text || '')[0] !== '/' && text) {
-
-        let text2 = text.replace(/(?:https?):\/\/t.me[\n\S]+/g, '');
-        let retext = text2.replace(/(?:@)[\n\S]+/g, 'BitWire Support');
-
-        //1,2
-        if (chatId.toString() === group1) {
-            await bot.sendMessage(group2, retext)
-        } 
-        else if (chatId.toString() === group2) {
-            await bot.sendMessage(group1, retext)
-        }
-
-        //3,4
-        if (chatId.toString() === group3) {
-            await bot.sendMessage(group4, retext)
-        } 
-        else if (chatId.toString() === group4) {
-            await bot.sendMessage(group3, retext)
-        }
-
-        //5,6
-        if (chatId.toString() === group5) {
-            await bot.sendMessage(group6, retext)
-        } 
-        else if (chatId.toString() === group6) {
-            await bot.sendMessage(group5, retext)
-        }
-
-        //test
-        if (chatId.toString() === user1) {
-            await bot.sendMessage(user2, retext)
-        } 
-        else if (chatId.toString() === user2) {
-            await bot.sendMessage(user1, retext)
-        }
-    }
+    
 
 });
 
@@ -157,7 +183,15 @@ bot.on('message', async (msg) => {
 const PORT = process.env.PORT || 8002;
 
 const start = async () => {
-    console.log('HTTPS Resender Bot running on port ' + PORT);
+    console.log('Resender Bot running on port ' + PORT);
+
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+    } catch (error) {
+        console.log("Подключение сломалось: ", error)
+    }
 }
 
 start()
