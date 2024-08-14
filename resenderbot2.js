31eb28be01d34424134f2e9b91bf2e7fe56f737f
Sergey Gornih <seregaland@yandex.ru>
2024-08-14 10:04:53 +0300

    fix

--- resenderbot2.js
+++ resenderbot2.js
@@ -43,18 +43,6 @@ const httpsServer = https.createServer(credentials, app);
 //function
 const sendMyMessage = require('./common/sendMyMessage')
 
-const user1 = process.env.GROUP12
-const user2 = process.env.GROUP22
-
-const group1 = process.env.GROUP1
-const group2 = process.env.GROUP2
-
-const group3 = process.env.GROUP3
-const group4 = process.env.GROUP4
-
-const group5 = process.env.GROUP5
-const group6 = process.env.GROUP6
-
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -66,7 +54,7 @@ bot.on('message', async (msg) => {
     const replyId = msg.reply_to_message ? msg.reply_to_message.message_id : '';
     
 
-    console.log(msg)
+    //console.log(msg)
 
     try {
         if (text === '/start') {
@@ -91,31 +79,11 @@ bot.on('message', async (msg) => {
             const doc = msg.document.file_id
             //console.log(image)
 
-            if (chatId.toString() === group1) {
-                await bot.sendDocument(group2, doc)
-            } 
-            else if (chatId.toString() === group2) {
-                await bot.sendDocument(group1, doc)
-            }
-
-            if (chatId.toString() === group3) {
-                await bot.sendDocument(group4, doc)
-            } 
-            else if (chatId.toString() === group4) {
-                await bot.sendDocument(group3, doc)
-            }
-
-            if (chatId.toString() === group5) {
-                await bot.sendDocument(group6, doc)
-            } 
-            else if (chatId.toString() === group6) {
-                await bot.sendDocument(group5, doc)
-            }
         }
 
         //обработка изображений
         if (msg.photo) {
-            console.log(msg.photo)
+            //console.log(msg.photo)
 
 
             //найти беседу
@@ -137,60 +105,14 @@ bot.on('message', async (msg) => {
                     const response = await bot.sendPhoto(exist.dataValues.members[1], image)
                     //сохранить сообщение в базе данных
                     const convId = await sendMyMessage(text, "image", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-
-                } else if (chatId.toString() === exist.dataValues.members[1]) {
-                    const response = await bot.sendPhoto(exist.dataValues.members[0], image)
-                     //сохранить сообщение в базе данных
-                    const convId = await sendMyMessage(text, "image", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-                }
-            } else {
-                if (chatId.toString() === group1) {
-                    await bot.sendPhoto(group2, image)
                 } 
-                else if (chatId.toString() === group2) {
-                    await bot.sendPhoto(group1, image)
-                }
-
-                if (chatId.toString() === group3) {
-                    await bot.sendPhoto(group4, image)
-                } 
-                else if (chatId.toString() === group4) {
-                    await bot.sendPhoto(group3, image)
-                }
-
-                if (chatId.toString() === group5) {
-                    await bot.sendPhoto(group6, image)
-                } 
-                else if (chatId.toString() === group6) {
-                    await bot.sendPhoto(group5, image)
-                }
-            }
+            } 
         }
 
         //обработка аудио сообщений
         if (msg.voice) {
             const voice = msg.voice.file_id
 
-            if (chatId.toString() === group1) {
-                await bot.sendVoice(group2, voice)
-            } 
-            else if (chatId.toString() === group2) {
-                await bot.sendVoice(group1, voice)
-            }
-
-            if (chatId.toString() === group3) {
-                await bot.sendVoice(group4, voice)
-            } 
-            else if (chatId.toString() === group4) {
-                await bot.sendVoice(group3, voice)
-            }
-
-            if (chatId.toString() === group5) {
-                await bot.sendVoice(group6, voice)
-            } 
-            else if (chatId.toString() === group6) {
-                await bot.sendVoice(group5, voice)
-            }
         }
 
         //обработка сообщений    
@@ -260,58 +182,12 @@ bot.on('message', async (msg) => {
                     const response = await bot.sendMessage(exist.dataValues.members[1], retext)
                     //сохранить сообщение в базе данных
                     const convId = await sendMyMessage(text, "text", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-
-                } else if (chatId.toString() === exist.dataValues.members[1]) {
-                    const response = await bot.sendMessage(exist.dataValues.members[0], retext)
-                     //сохранить сообщение в базе данных
-                    const convId = await sendMyMessage(text, "text", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-                }
+                } 
             }
-            
-
-            //1,2
-            if (chatId.toString() === group1) {
-                await bot.sendMessage(group2, retext)
-            } 
-            else if (chatId.toString() === group2) {
-                await bot.sendMessage(group1, retext)
-            }
-
-            //3,4
-            if (chatId.toString() === group3) {
-                await bot.sendMessage(group4, retext)
-            } 
-            else if (chatId.toString() === group4) {
-                await bot.sendMessage(group3, retext)
-            }
-
-            //5,6
-            if (chatId.toString() === group5) {
-                await bot.sendMessage(group6, retext)
-            } 
-            else if (chatId.toString() === group6) {
-                await bot.sendMessage(group5, retext)
-            }
-
-            //test
-            // if (chatId.toString() === user1) {
-            //     const response = await bot.sendMessage(user2, retext)
-            //     //console.log(response)
-            //     //сохранить сообщение в базе данных
-            //     const convId = await sendMyMessage(text, "text", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-            // } 
-            // else if (chatId.toString() === user2) {
-            //     const response = await bot.sendMessage(user1, retext)
-            //     //console.log(response)
-            //     //сохранить сообщение в базе данных
-            //     const convId = await sendMyMessage(text, "text", fromId, chatId, groupTitle, false, parseInt(response.message_id)-1, replyId)
-            // }
         }
     } catch (error) {
         console.log('Произошла непредвиденная ошибка в боте Resender! ', error.message)
     }
-
-    
 
 });
 
