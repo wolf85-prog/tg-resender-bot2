09eb823d893da2a6c5d4760c77420e3d1bdbe1c7
Sergey Gornih <seregaland@yandex.ru>
2024-08-15 17:38:48 +0300

    fix

---
+++ controllers/userbotController.js
@@ -0,0 +1,85 @@
+const {UserBot} = require('../models/models')
+
+class UserbotController {
+
+    async addUser(req, res) {       
+        try {    
+            let exist=await User.findOne( {sub: request.body.sub})
+            
+            if(exist){
+                response.status(200).json({msg: "user already exist"});
+                return;
+            }
+
+            const {first_name, last_name, chatId} = req.body
+
+            const newUser = await UserBot.create({first_name, last_name, chatId})
+            return res.status(200).json(newUser);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    async editUser(req, res) { 
+        const {id} = req.params      
+        try {    
+            let exist=await UserBot.findOne( {where: {chatId: id}} )
+            
+            if(!exist){
+                res.status(500).json({msg: "user not exist"});
+                return;
+            }
+
+            const {username} = req.body
+
+            const newUser = await UserBot.update(
+                { username },
+                { where: {chatId: id} })
+            return res.status(200).json(newUser);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    async editUserAvatar(req, res) { 
+        const {id} = req.params      
+        try {    
+            let exist=await UserBot.findOne( {where: {chatId: id}} )
+            
+            if(!exist){
+                res.status(500).json({msg: "user not exist"});
+                return;
+            }
+
+            const {avatar} = req.body
+
+            const newUser = await UserBot.update(
+                { avatar },
+                { where: {chatId: id} })
+            return res.status(200).json(newUser);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            const users = await UserBot.findAll()
+            return res.status(200).json(users);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    async getUser(req, res) {
+        const {id} = req.params
+        try {
+            const userbot = await UserBot.findOne({where: {chatId: id}})
+            return res.status(200).json(userbot);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+}
+
+module.exports = new UserbotController()
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = new Router()
 
 const userController = require('../controllers/userController')
 const conversationController = require('../controllers/conversationController')
-
+const userbotController = require('../controllers/userbotController')
 
 // get USERS  
 router.get("/users", userController.getUsers);
@@ -13,5 +13,8 @@ router.get("/users", userController.getUsers);
 router.post('/conversation/add', conversationController.newConversation)
 router.get('/conversation/get/:id', conversationController.getConversation)
 router.get('/conversations/get', conversationController.getConversations)
+
+router.get('/userbots/get', userbotController.getUsers)
+router.get('/userbots/get/:id', userbotController.getUser)
 
 module.exports = router
